feec5dbf7f3b00b3ef25bd9025a5636129328fd6
Антон <Balamut86@list.ru>
2023-05-30 20:00:54 +0300

    Добавьте файлы проекта.

---
+++ ITMO.WinCSCourse2023.Lab02.task06_BiblForm/IPubs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ITMO.WinCSCourse2023.Lab02.task06_BiblForm
+{
+    interface IPubs
+    {
+        
+       void Subs ();
+                
+    }
+}
---
+++ ITMO.WinCSCourse2023.Lab02.task07_WinTimer1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.WinCSCourse2023.Lab02.task07_WinTimer1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e, UserControlTimer userControlTimer)
+        {
+            if (userControlTimer1.TimeEnabled) 
+            {
+            userControlTimer1.TimeEnabled = false;
+                button1.Text = "START";
+            }
+            else
+            {
+                userControlTimer1.TimeEnabled = true;
+                button1.Text = "STOP";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (userControlTimer1.TimeEnabled) 
+            {
+             userControlTimer1.TimeEnabled=false;
+                button1.Text = "START";
+            }
+            else 
+            { userControlTimer1.TimeEnabled=true;
+                button1.Text = "STOP";
+            }
+        }
+    }
+}
